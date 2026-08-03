0cc78f764c76e9cc018586ea63656630321d7bd7
KathanCodes <zulakathan06@gmail.com>
2026-08-03 13:48:42 +0530

    Add: two-integer-sum - submission-1

---
+++ Data Structures & Algorithms/two-integer-sum/submission-1.py
@@ -0,0 +1,12 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        
+        hashSet = defaultdict()
+
+        for index , num in enumerate(nums) :
+            if target - num in hashSet :
+                return [hashSet[target-num],index]
+
+            hashSet[num] = index
+
+        return NaN
